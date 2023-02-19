c2ceaa86f69518e3b46a6b16bbc035345efcba6a
evgen <parom.evgen@mail.ru>
2023-02-19 14:55:49 +0300

    request session parameters, expression language, scopes

--- src/com/parom/servlet/ContentServlet.java
+++ src/com/parom/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package com.parom.servlet;
 
+import com.parom.dto.FlightDTO;
+import com.parom.service.FlightService;
 import com.parom.util.JSPHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,12 +10,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
 
+    private final FlightService flightService = FlightService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List<FlightDTO> flightDTOs = flightService.findAll();
+
+        req.setAttribute("flights", flightDTOs);
+        req.getSession().setAttribute("flightsMap", flightDTOs.stream().collect(toMap(FlightDTO::getId, FlightDTO::getDescription)));
+
         req.getRequestDispatcher(JSPHelper.getPath("content")).forward(req, resp);
     }
 }
--- web/WEB-INF/jsp/content.jsp
+++ web/WEB-INF/jsp/content.jsp
@@ -6,7 +6,16 @@
 <body>
     <%@ include file="head.jsp" %>
     <div>
-        Content. Русский
+        <span>Content. Русский</span>
+        <p>Size: ${requestScope.flights.size()}</p>
+        <p>Id: ${requestScope.flights.get(0).id}</p>
+        <p>Id null save: ${requestScope.flights[1].id}</p>
+        <p>Id null save session scope: ${not empty sessionScope.flightsMap}</p>
+        <p>JSESSIONID: ${cookie["JSESSIONID"].value}, unique identifier</p>
+        <p>Header: ${header["Cookie"]}</p>
+        <p>Param id: ${param.id}</p>
+        <p>Param test: ${param.test}</p>
+
     </div>
     <%@include file="footer.jsp" %>
 </body>
